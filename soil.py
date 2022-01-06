d39dbc32b1a39f314401e8e4f0d1941c917bb385
Prof-Krapu <96750177+Prof-Krapu@users.noreply.github.com>
2022-01-06 14:02:52 +0100

    Create soil.py

---
+++ soil.py
@@ -0,0 +1,20 @@
+from machine import Pin, ADC            #importing Pin and ADC class
+from time import sleep                  #importing sleep class
+
+adc = ADC(Pin(33))            #creating potentiometer object
+adc.atten(ADC.ATTN_11DB)       #3.3V full range of voltage
+def remap(value, leftMin, leftMax, rightMin, rightMax):
+    # Figure out how 'wide' each range is
+    leftSpan = leftMax - leftMin
+    rightSpan = rightMax - rightMin
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - leftMin) / float(leftSpan)
+    # Convert the 0-1 range into a value in the right range.
+    # return int(rightMin + (valueScaled * rightSpan))
+    return rightMin + (valueScaled * rightSpan)
+while True:
+    soil = adc.read()   #reading analog pin
+    print(soil)#printing the ADC value
+    soil2 = remap(soil, 3344,1780,0,100)
+    print(soil2)
+    sleep(0.25)
